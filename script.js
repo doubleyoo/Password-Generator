2a32b92352dd6080ff8d9c459135757090a86025
Double Yoo <doubleyoo@Doubles-MBP.lan1>
2021-02-18 10:14:55 -0500

    updating homework3

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 // Assignment Code
+console.log("Hello")
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  console.log("Inside write password")
+  prompt("What type of password do you want, doc?");
+  var password = function generatePassword(){
+    console.log("Inside the generate password method")
+  };
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
