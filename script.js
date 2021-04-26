a2aba2221d933e8b2a33c247b20dc52bfba7e40d
Double Yoo <doubleyoo@Doubles-MacBook-Pro.local>
2021-04-26 12:50:56 -0400

    updating js file

--- script.js
+++ script.js
@@ -1,64 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// var prompt
-// var
-
-// function getRandomIndex(array) {
-//   var index = Math.floor(Math.random() * array.length)
-//   var element = array[index]
-//   return element;
-// }
 
 // Write password to the #password input
-// function writePassword() {
-//   const string = [];
-//   var password = []
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-//   }
-
-
-
-  // if (custPwLength <= 128 && custPwLength >= 8) {
-  //   var upper = prompt('Would you like to include upper, Y/N?')
-  //   // var lower = prompt('Would you like to include lower, Y/N?')
-  //   // var special = prompt('Would you like to include special, Y/N?')
-  //   // var number =  prompt('Would you like to include number, Y/N?')
-
-
-  //   if (upper === 'Y') {
-  //     string.push('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
-  //   }
-
-  //   generatePassword()
-
-  // }
-
-
-
-  // var passwordLength = prompt("Bruh, how long do you want your password to be (8-128 characters)")
-  // console.log("passwordLength: ", passwordLength)
-  // // var password = generatePassword();
-  // // var passwordText = document.querySelector("#password");
-
-  // // passwordText.value = password;
-
-// }
-function generatePassword(){
-  // create a parent function that orchestrates all the methods required to generate the password
-  return getPasswordSpecs().then(res=>{
-    return generatePassword(res)
-  })
+  passwordText.value = password;
 
 }
 
-function getPasswordSpecs() {
-  var custPwLength = parseInt(prompt("Bruh, how long do you want your password to be (8-128 characters)"))
-  if (custPwLength > 128 || custPwLength < 8) {
-    alert('Password must be between 8 & 128 characters')
-    return
-  }
-
-
+function getCharSet() {
   var specialCharacters = confirm("Do you want special characters?")
   var numericCharacters = confirm("Do you want numeric characters?")
   var upperCaseCharacters = confirm("Do you want upper case characters?")
@@ -67,29 +19,38 @@ function getPasswordSpecs() {
     alert('Must slect at least one character type.')
     return
   }
-  var specs = {
-    passwordLength: custPwLength,
-    specialCharacters: specialCharacters,
-    upperCaseCharacters: upperCaseCharacters,
-    lowerCaseCharacters: lowerCaseCharacters,
-    numericCharacters: numericCharacters
-  }
-  console.log(specs)
-  return specs
+  var charSet = ""
+  if (specialCharacters === true) charSet += '!@#$%^&*()'
+  if (numericCharacters === true) charSet += '0123456789'
+  if (upperCaseCharacters === true) charSet += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  if (lowerCaseCharacters === true) charSet += 'abcdefghijklmnopqrstuv'
+
+  return charSet
+
 }
 
-function generatePassword(specs) {
-  console.log(specs)
-  // var specs = getPasswordSpecs()
-  // var i;
-  // for (i = 0; i < custPwLength; i++) {
-  //   console.log(string[getRandomInt(string.length)])
-  // }
+function getRandomIndex(array) {
+  var index = Math.floor(Math.random() * array.length)
+  var element = array[index]
+  return element;
+}
+
+function generatePassword() {
+  var custPwLength = parseInt(prompt("Bruh, how long do you want your password to be (8-128 characters)"))
+  if (custPwLength > 128 || custPwLength < 8 || isNaN(custPwLength)) {
+    alert('Password length must be a number between 8 & 128')
+    return 
+  }
+
+  var charSet = getCharSet()
+  var i;
+  var password = "" 
+  for (i = 0; i < custPwLength; i++) {
+    password += getRandomIndex(charSet) 
+  } 
+  return password
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword);
-
-
-
+generateBtn.addEventListener("click", writePassword);
